cea9fcda24e0c9d95b573ee91c344108abf0ba94
rishworld2050 <rishworld2050@gmail.com>
2017-05-25 10:55:24 +0530

    Added live stock report project

---
+++ Stock/Scripts/orderCart.js
@@ -0,0 +1,24 @@
+var stkApp=angular.module("stockApp",['ngWebSocket']);
+
+  stkApp.factory('MyData',["$websocket", function($websocket) {
+      // Open a WebSocket connection
+      var dataStream = $websocket('ws://stocks.mnet.website');
+
+      var collection = [];
+
+      dataStream.onMessage(function(message) {
+        collection.push(JSON.parse(message.data));
+      });
+
+      var methods = {
+        collection: collection,
+        get: function() {
+          dataStream.send(JSON.stringify({ action: 'get' }));
+        }
+      };
+
+      return methods;
+    }]);
+stkApp.controller('SomeController', ["MyData","$scope",function ($scope, MyData) {
+      $scope.MyData = MyData;
+    }]);
