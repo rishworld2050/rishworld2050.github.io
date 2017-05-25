0bf0a12bb72e07e0ed67f4832af57d55d6b1ccb2
rishworld2050 <rishworld2050@gmail.com>
2017-05-25 15:42:10 +0530

    updates

--- Stock/Scripts/orderCart.js
+++ Stock/Scripts/orderCart.js
@@ -7,8 +7,7 @@ var stkApp=angular.module("stockApp",['ngWebSocket']);
       var collection = [];
 
       dataStream.onMessage(function(message) {
-        collection.push(JSON.parse(message.data));
-        console.log(JSON.parse(message.data));
+      	collection[0]=JSON.parse(message.data);
       });
 
       var methods = {
@@ -20,6 +19,49 @@ var stkApp=angular.module("stockApp",['ngWebSocket']);
 
       return methods;
     }]);
-stkApp.controller('SomeController', ["MyData","$scope",function ($scope, MyData) {
-      $scope.MyData = MyData;
-    }]);
+stkApp.controller('StockController', ["MyData","$scope",function (MyData, $scope) {
+     
+     	$scope.stockData={};
+     $scope.test=MyData;
+    setInterval(function(){
+   // console.log($scope.test.collection);
+   $scope.processData(angular.copy($scope.test.collection[0]));
+ },3000);
+ 
+ $scope.processData=function(dataArray){
+ 	dataArray.forEach(([name,price]) => {
+
+ 		if(!$scope.stockData.hasOwnProperty(`${name}`)){
+
+	$scope.stockData[`${name}`]={
+               "name":`${name}`,
+               "price":parseFloat(`${price}`),
+               "color":"white",
+               "delta":"0",
+               "timestamp":new Date().toLocaleString()
+	};
+}
+
+else{
+if(parseFloat(`${price}`)!==$scope.stockData[`${name}`].price){
+	$scope.stockData[`${name}`].color=parseFloat(`${price}`)<$scope.stockData[`${name}`].price? "red":"green";
+	$scope.stockData[`${name}`].delta=parseFloat(`${price}`)<$scope.stockData[`${name}`].price? $scope.calDownValue(parseFloat(`${price}`),$scope.stockData[`${name}`].price):$scope.calUpValue(parseFloat(`${price}`),$scope.stockData[`${name}`].price);
+$scope.stockData[`${name}`].price=parseFloat(`${price}`);
+$scope.stockData[`${name}`].timestamp=new Date().toLocaleString();
+}
+}
+
+});
+ 	console.log($scope.stockData);
+ 	}
+ 	$scope.calDownValue=function(price,prevPrice){
+       return "-"+parseFloat(((prevPrice-price)/prevPrice)*100).toFixed(2)+"%";
+        	}
+$scope.calUpValue=function(price,prevPrice){
+       return "+"+parseFloat(((price-prevPrice)/prevPrice)*100).toFixed(2)+"%";
+ 	}
+
+ 
+}
+
+    ]);
